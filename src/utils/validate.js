650412c3c7dd1db0ff4741780542563944bc747b
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-16 11:13:25 +0530

    Validate Name added

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,4 @@
-export const checkValidData = (email, password) => {
+export const checkValidData = (email, password, name) => {
   // if this regex is pass then it will return TRUE inside it or FALSE inside "isEmailValid"
   const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
     email
@@ -8,12 +8,12 @@ export const checkValidData = (email, password) => {
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-  // const isNameValid = /\b([A-Z][-,a-z. ']+[ ]*)+/.test(name)
+  const isNameValid = /\b([A-Z][-,a-z. ']+[ ]*)+/.test(name);
 
   // return's Error Message
   if (!isEmailValid) return "Email ID is not valid";
   if (!isPasswordValid) return "Password is not valid";
-  // if (!isNameValid) return "Name is not valid";
+  if (!isNameValid) return "Name is not valid";
 
   //if both are valid then return null i.e. No Error
   return null;
