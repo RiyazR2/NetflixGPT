85d5d6a205fa4b10805760fc715237f2003e3262
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-20 09:30:24 +0530

    Trailer and movie description

---
+++ src/components/MainContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import BackgroundVideo from "./BackgroundVideo";
+
+const MainContainer = () => {
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  if (!movies) return; // AKA Early Return
+
+  const mainMovie = movies[0];
+  console.log("Main Movie", mainMovie);
+  const { original_title, overview, id } = mainMovie;
+
+  return (
+    <div>
+      <VideoTitle title={original_title} overview={overview} />
+      <BackgroundVideo movieId={id} />
+    </div>
+  );
+};
+
+export default MainContainer;
