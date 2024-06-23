5fcb3f9b4a13fe4060ad74e93e72ba0d290a99e8
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-24 01:27:25 +0530

    SUPPORTED_LANGUAGES

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,7 +4,7 @@ export const NETFLIX_LOGO =
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-export const BG_URL =
+export const BG_IMG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/51c1d7f7-3179-4a55-93d9-704722898999/be90e543-c951-40d0-9ef5-e067f3e33d16/IN-en-20240610-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 // "https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg"
 
@@ -21,3 +21,12 @@ export const API_OPTIONS = {
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780/";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "marathi", name: "Marathi" },
+  { identifier: "telgu", name: "Telgu" },
+  { identifier: "tamil", name: "Tamil" },
+  { identifier: "urdu", name: "Urdu" },
+];
