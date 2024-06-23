76b2e1e3a6656b5116906d668248a2bd8a946644
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-24 01:25:35 +0530

    SUPPORTED_LANGUAGES

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,28 @@
+const lang = {
+  en: {
+    search: "Search",
+    gptSearchPlaceholder: "What would you like to watch today?",
+  },
+  hindi: {
+    search: "खोज",
+    gptSearchPlaceholder: "आज आप क्या देखना चाहेंगे?",
+  },
+  marathi: {
+    search: "शोधा",
+    gptSearchPlaceholder: "तुम्हाला आज काय बघायला आवडेल?",
+  },
+  telgu: {
+    search: "శోధ",
+    gptSearchPlaceholder: "మీరు ఈరోజు ఏమి చూడాలనుకుంటున్నారు?",
+  },
+  tamil: {
+    search: "சோதா",
+    gptSearchPlaceholder: "இன்று நீங்கள் எதைப் பார்க்க விரும்புகிறீர்கள்?",
+  },
+  urdu: {
+    search: "تلاش کریں",
+    gptSearchPlaceholder: "آج آپ کیا دیکھنا چاہیں گے؟",
+  },
+};
+
+export default lang;
