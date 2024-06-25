4b0074bff305615691b0d88671ee9dc8d20661ac
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-25 14:32:46 +0530

    GPT Search Page

--- src/components/gpt_components/GptSearchPage.js
+++ src/components/gpt_components/GptSearchPage.js
@@ -1,7 +1,18 @@
 import React from "react";
+import GptSearchBar from "./GptSearchBar";
+import GptMoviesSuggestions from "./GptMoviesSuggestions";
+import { BG_IMG_URL } from "../../utils/constant";
 
 const GptSearch = () => {
-  return <div>GptSearch</div>;
+  return (
+    <div>
+      <div className="absolute -z-10">
+        <img src={BG_IMG_URL} alt="Background" />
+      </div>
+      <GptSearchBar />
+      <GptMoviesSuggestions />
+    </div>
+  );
 };
 
 export default GptSearch;
