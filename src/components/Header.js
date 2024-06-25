e54c4de7e48c337c9d3bc01bf8c78fa2eddb2793
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-25 14:36:11 +0530

    Language Added for GPT Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { NETFLIX_LOGO } from "../utils/constant";
+import { NETFLIX_LOGO, SUPPORTED_LANGUAGES } from "../utils/constant";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -45,6 +48,16 @@ const Header = () => {
     return () => unsubscribe(); // it will Remove onAuthStateChange from our Browsers when our component unload/unmount
   }, []);
 
+  // Toggle GptSearch
+  const handleGptSearchClick = () => {
+    // console.log("GPT Button Clicked");
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b  from-black z-10 flex justify-between">
       <img className="w-44 " src={NETFLIX_LOGO} alt="NETFLIX_LOGO" />
@@ -52,6 +65,24 @@ const Header = () => {
       {/* only show when user is logged in */}
       {user && (
         <div className="flex p-2">
+          {showGptSearch && (
+            <select
+              className="bg-gray-900 text-white p-2 m-2 rounded-lg"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option value={lang.identifier} key={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch ? "Home" : "GPT Search"}
+          </button>
           <img className="w-12 h-12 mx-2" src={user?.photoURL} alt="AVATAR" />
           <p className="font-bold text-white p-4">{user?.displayName}</p>
           <button className="font-bold text-white" onClick={handleSignOut}>
