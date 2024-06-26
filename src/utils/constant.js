47bc956ba4ba9d1ccb61c91a151b5615fb49cd9c
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-26 11:46:02 +0530

    Updated OPENAO_KEY

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -15,12 +15,14 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjViOTMwYmVmZGFjNWM2MTNjODE1ODBiMGY5NmEzNSIsInN1YiI6IjY2NzFkNjE3Nzc4M2ExNmQ5MmYzMDE0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._-JOsMWx6vKF7I2VgPq1zKW1_ujQX7Sl4niRqkGSLfk",
+
+    Authorization: "Bearer" + process.env.REACT_APP_TMDB_KEY,
   },
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780/";
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
 
 export const SUPPORTED_LANGUAGES = [
   { identifier: "en", name: "English" },
