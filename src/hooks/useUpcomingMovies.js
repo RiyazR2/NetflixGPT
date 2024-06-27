407dac1e19f23ca02d40f102c94f9a335eb94c80
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-27 09:43:14 +0530

    Memoization

--- src/hooks/useUpcomingMovies.js
+++ src/hooks/useUpcomingMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constant";
 import { addUpcomingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,6 +6,10 @@ import { useEffect } from "react";
 const useUpcomingMovies = () => {
   // Fetching Now Playing Movies from TMDB and putting it into the Store
   const dispatch = useDispatch();
+
+  // Doing for Memoization
+  const upcomingMovies = useSelector((store) => store.movies.upcomingMovies);
+
   const getUpcomingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/upcoming?page=1",
@@ -17,7 +21,9 @@ const useUpcomingMovies = () => {
   };
 
   useEffect(() => {
-    getUpcomingMovies();
+    // call only when upcomingMovies is not present(memoization)
+    // if (!upcomingMovies) getUpcomingMovies();
+    !upcomingMovies && getUpcomingMovies();
   }, []);
 };
 
