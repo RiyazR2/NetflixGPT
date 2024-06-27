b40c9f5a4fdc19ff67262acbe922416a56991e74
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-27 09:42:09 +0530

    Memoization

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constant";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,6 +6,12 @@ import { useEffect } from "react";
 const useNowPlayingMovies = () => {
   // Fetching Now Playing Movies from TMDB and putting it into the Store
   const dispatch = useDispatch();
+
+  // Doing for Memoization
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
+
   const getNowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
@@ -17,7 +23,9 @@ const useNowPlayingMovies = () => {
   };
 
   useEffect(() => {
-    getNowPlayingMovies();
+    // call only when nowPlayingMovies is not present(memoization)
+    // if (!nowPlayingMovies) getNowPlayingMovies();
+    !nowPlayingMovies && getNowPlayingMovies(); // same as above code, both are same
   }, []);
 };
 
