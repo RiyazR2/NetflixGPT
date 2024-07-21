447c6c20d490b5784bcc1d25a8ba9666ca074b1e
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-21 12:09:33 +0530

    CardInfo Path added

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,16 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
 
+import CardInfo from "./gpt_components/CardInfo";
+
 const Body = () => {
   const appRouter = createBrowserRouter([
     { path: "/", element: <Login /> },
     { path: "/browse", element: <Browse /> },
+    {
+      path: "/cardinfo/:id",
+      element: <CardInfo />,
+    },
   ]);
 
   return (
