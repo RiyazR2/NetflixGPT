24df436ef73611252dbd383cf4055c066bdad160
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-23 21:26:25 +0530

    constant

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -17,16 +17,13 @@ export const API_OPTIONS = {
   headers: {
     accept: "application/json",
 
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjViOTMwYmVmZGFjNWM2MTNjODE1ODBiMGY5NmEzNSIsInN1YiI6IjY2NzFkNjE3Nzc4M2ExNmQ5MmYzMDE0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._-JOsMWx6vKF7I2VgPq1zKW1_ujQX7Sl4niRqkGSLfk",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
 
 export const TMDB_IMG_CDN_URL = "https://image.tmdb.org/t/p/w780/";
 
-// export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
-export const OPENAI_KEY =
-  "sk-proj-SG4AyYUBDEqfZtAdeBnXT3BlbkFJCebyh6mUcZVvT0DS4Smo";
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
 
 export const SUPPORTED_LANGUAGES = [
   { identifier: "en", name: "English" },
