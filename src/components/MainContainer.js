76f395bafdfe9963fc5ea092fe84b5b6b2bb285a
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-23 16:44:16 +0530

    Updated Responsive Page

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,22 +4,22 @@ import VideoTitle from "./VideoTitle";
 import BackgroundVideo from "./BackgroundVideo";
 
 // MainContainer Contain on 1 movie to show movie title and background trailer
-const MainContainer = () => {
+const MainContainer = React.memo(() => {
   // this movies contain 20 movies but we only need one
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
   if (!movies) return; // AKA Early Return
 
-  const mainMovie = movies[1];
+  const mainMovie = movies[0];
   console.log("Main Movie", mainMovie);
   const { original_title, overview, id } = mainMovie;
 
   return (
-    <div className=" bg-black ">
+    <div className="pt-[30%] md:pt-0 bg-black ">
       {console.log("MainContainer")}
       <VideoTitle title={original_title} overview={overview} />
       <BackgroundVideo movieId={id} />
     </div>
   );
-};
+});
 
 export default MainContainer;
